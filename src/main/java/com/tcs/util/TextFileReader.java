e7542567f49d6d3bdac9e40a4028728ca409806c
asit <asit2happy@gmail,com>
2019-03-04 23:08:57 +0530

    Created

---
+++ src/main/java/com/tcs/util/TextFileReader.java
@@ -0,0 +1,35 @@
+package com.tcs.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TextFileReader {
+       public String[] getOrderId() throws IOException {
+				String fileName = "test.txt";
+				File file = new File(fileName);
+				FileReader fr = new FileReader(file);
+				BufferedReader br = new BufferedReader(fr);
+				String line;
+				String[] value=new String[5];
+				int i=0;
+				while((line = br.readLine()) != null){
+					
+				    //process the line
+					value[i]=line.substring(0, line.indexOf(","));
+					i++;
+				}
+				
+				for(int j=0;j<value.length;j++) {
+					System.out.println(value[j]);
+				}
+				return value;
+
+	}
+public static void main(String[] s) throws IOException {
+	TextFileReader tfr=new TextFileReader();
+	tfr.getOrderId();
+}
+}
---
+++ src/main/java/com/tcs/util/XMLModifier.java
@@ -0,0 +1,98 @@
+package com.tcs.util;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+/**
+ * 
+ * @author ASIT KUMAR PANDA
+ * 
+ *
+<webServiceRequestEnvelope>
+	<versionedIdentifier>
+		<value>BT5X87P3-80-78_1</value>
+		<source>Aib</source>
+		<version>2</version>
+	</versionedIdentifier>
+</webServiceRequestEnvelope>
+
+ *
+ */
+public class XMLModifier {
+
+	public void xmlModifierUtil() throws IOException {
+
+		TextFileReader tfr=new TextFileReader();
+		for(int i=0;i<tfr.getOrderId().length;i++) {
+		String orderid=tfr.getOrderId()[i];
+
+		
+		 String filePath = "demo.xml";
+	        File xmlFile = new File(filePath);
+	        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder dBuilder;
+	        try {
+	            dBuilder = dbFactory.newDocumentBuilder();
+	            Document doc = dBuilder.parse(xmlFile);
+	            doc.getDocumentElement().normalize();
+	            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+	            NodeList nodeList = doc.getElementsByTagName("versionedIdentifier");
+	          
+	            if (nodeList.item(0).getNodeType() == Node.ELEMENT_NODE) {
+		            Element element = (Element) nodeList.item(0);
+		            setTagValue("value", element,orderid);
+	            }
+	        
+	        
+	        //The following logic is to write into new xmlfile
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File("E:\\output\\oDemo"+orderid+".xml"));
+			transformer.transform(source, result);
+
+			System.out.println("Done");
+
+		   } catch (ParserConfigurationException pce) {
+			pce.printStackTrace();
+		   } catch (TransformerException tfe) {
+			tfe.printStackTrace();
+		   } catch (IOException ioe) {
+			ioe.printStackTrace();
+		   } catch (SAXException sae) {
+			sae.printStackTrace();
+		   }
+
+		}
+	
+	}
+	public static void main(String[] args) throws IOException {
+		
+		XMLModifier oXMLModifier=new XMLModifier();
+		oXMLModifier.xmlModifierUtil();
+	}
+
+
+	  private static void setTagValue(String tag, Element element,String value) {
+	        NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
+	        Node node = (Node) nodeList.item(0);
+	        String temp=node.getNodeValue().substring(node.getNodeValue().indexOf("-"),node.getNodeValue().length());
+	        node.setTextContent(value+temp);
+	    }
+	  
+	  
+}
